fefe6e5f9cb22376352cf37d17cad91c37d227ae
Jose-Monagas <josemonagas212@gmail.com>
2021-01-09 23:49:07 -0500

    adding files to Mapping_Multiple_Points

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,16 +2,49 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: 'black',
-    fillColor: 'yellow'
+// Get data from cities.js
+let cityData = cities;
 
- }).addTo(map);
+// Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.marker(city.location).addTo(map)
+//     .bindPopup(`<h2> ${city.city} , ${city.state} </h2> <hr> <h3> Population ${city.population.toLocaleString()} </h3>`)
+//     .addTo(map)
+//    });
+
+// Loop through the cities array and create one circle marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location, {
+//         radius: city.population/100000
+//     })
+//     .bindPopup(`<h2> ${city.city} , ${city.state} </h2> <hr> <h3> Population ${city.population.toLocaleString()} </h3>`)
+//     .addTo(map)
+//     });
+
+// Loop through the cities array and create one marker (circle) for each city SKILL DRILL.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/200000,
+        color: "orange",
+        weight: 4
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
 
 // We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
+
+// Skill Drill
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
